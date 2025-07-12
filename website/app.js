3e533e430ddafb69a93f161d1a74501500cbd7f8
tankist256 <mrmarmuk69@gmail.com>
2025-07-12 20:20:21 +0300

    add to cart item

--- website/app.js
+++ website/app.js
@@ -8,4 +8,26 @@ const catalog = {
     3: { id: 3, name: "Sweater", price: 60 },
     4: { id: 4, name: "Hoodie", price: 120 }
 };
+function UpdateCartUI() {
+    const cartStatus = document.querySelector(".cart-status");
+    if (cartState.items.length === 0) {
+        cartStatus.innerHTML = "<div>Your shopping cart is empty</div>";
+    } else {
+        const itemList = cartState.items.map(item => 
+            `<div>${item.name} - $${item.price}</div>`
+        ).join("");
+        cartStatus.innerHTML = `
+            <div>You have ${cartState.items.length} item(s) in your cart:</div>
+            ${itemList}
+        `;
+    }
+}
+function handleAddToCart(event, itemId) {
+    event.preventDefault();
+    const item = catalog[itemId];
+    if (item) {
+        cartState.items.push(item);
+        updateCartUI();
+    }
+}
 
